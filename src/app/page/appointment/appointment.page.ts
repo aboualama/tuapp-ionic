f6fe0fe063d0cde6c0f9af7f52be07364d788b5c
egyangel93 <egyangel93@gmail.com>
2019-11-08 21:10:56 +0100

    "first beta"

--- src/app/page/appointment/appointment.page.ts
+++ src/app/page/appointment/appointment.page.ts
@@ -10,7 +10,8 @@ import * as moment from 'moment/moment';
 
 import {Auth} from 'aws-amplify';
 
-import { DatePicker } from '@ionic-native/date-picker/ngx';
+import {DatePicker} from '@ionic-native/date-picker/ngx';
+import {SlotService} from '../../calender/slot.service';
 
 @Component({
     selector: 'app-appointment',
@@ -20,7 +21,6 @@ import { DatePicker } from '@ionic-native/date-picker/ngx';
 export class AppointmentPage implements OnInit {
 
 
-    
     public clientid: any;
     public serviceid: any;
     public servicetitle: any;
@@ -33,93 +33,101 @@ export class AppointmentPage implements OnInit {
     public all_times: any;
     public times: any;
     public status: false;
-    public service_duration: any; 
-    public slot: any; 
-    
+    public service_duration: any;
+    public slot: any;
 
-    constructor(private activatedRoute: ActivatedRoute, private modalController: ModalController, private router: Router, private datePicker: DatePicker) {
+
+    constructor(private activatedRoute: ActivatedRoute, private modalController: ModalController
+        , private router: Router, private datePicker: DatePicker, public slotService: SlotService) {
         this.moment = moment();
     }
 
     async ngOnInit() {
-
-    // Get service 
+        console.log(this.slotService.getTimeSlots([[600, 670]], 540, 1080, true, 'half', false, true));
+        // Get service
         const Id = this.activatedRoute.snapshot.paramMap.get('myid');
         const service = await API.graphql(graphqlOperation(Queries.getService, {id: Id}));
         this.serviceid = await service.data.getService.id;
         this.servicetitle = service.data.getService.title;
-        this.serviceprice = service.data.getService.price; 
-        this.service_duration = service.data.getService.duration; 
+        this.serviceprice = service.data.getService.price;
+        this.service_duration = service.data.getService.duration;
 
-    // Get user 
-        let user = await Auth.currentAuthenticatedUser();
+        // Get user
+        const user = await Auth.currentAuthenticatedUser();
         const {attributes} = user;
-        this.clientid = await attributes.sub; 
-      
-    // Get Setting 
-        const App_Id = await API.graphql(graphqlOperation(Queries.getApp, {id: AppId}));
-        this.start_times = await App_Id.data.getApp.settings.items[0].start_time;
-        this.end_times = await App_Id.data.getApp.settings.items[0].end_times;
-        this.calender_offset = await App_Id.data.getApp.settings.items[0].calender_offset; 
+        this.clientid = await attributes.sub;
 
-    // Get slot if condition  
+        // Get Setting
+        const App_Id = await API.graphql(graphqlOperation(Queries.getApp, {id: AppId}));
+        this.start_times = '09:00'; // await App_Id.data.getApp.settings.items[0].start_time;
+        this.end_times = '18:00'; // await App_Id.data.getApp.settings.items[0].end_times;
+        this.calender_offset = 30; // await App_Id.data.getApp.settings.items[0].calender_offset;
+
+        // Get slot if condition
         const slot = this.calender_offset < this.service_duration ? this.calender_offset : this.service_duration;
-       
+
         this.getTime(slot);
         this.getAppointment();
     }
 
 
     // Get Time
-    async getTime(slot) {  
-        console.log(slot + 'eslot'); 
+    async getTime(slot) {
+        console.log(slot + 'eslot');
 
-        function timeToArray(start, end, slot){
-            start = start.split(":");
-            end = end.split(":"); 
+        function timeToArray(start, end, slot) {
+            start = start.split(':');
+            end = end.split(':');
             start = parseInt(start[0]) * 60 + parseInt(start[1]);
             end = parseInt(end[0]) * 60 + parseInt(end[1]);
-        
-            let result = []; 
-            for (let  time = start; time <= end; time+=slot){
-                result.push( timeString(time));
-            } 
+
+
+            const result = [];
+            for (let time = start; time <= end; time += slot) {
+                result.push(timeString(time));
+            }
             return result;
-        }  
-        function timeString(time: number){
-            let hours: any; 
+        }
+
+        function timeString(time: number) {
+            let hours: any;
             let minutes: any;
             hours = Math.floor(time / 60);
-            minutes = time % 60; 
-            if (hours < 10) hours = "0" + hours;  
-            if (minutes < 10) minutes = "0" + minutes; 
-            return hours + ":" + minutes;
-        } 
-        let start   = "09:00" ;
-        let end     = "18:00" ; 
-        this.all_times = timeToArray(start, end, slot); 
- 
-    } 
- 
+            minutes = time % 60;
+            if (hours < 10) {
+                hours = '0' + hours;
+            }
+            if (minutes < 10) {
+                minutes = '0' + minutes;
+            }
+            return hours + ':' + minutes;
+        }
+
+        const start = '09:00';
+        const end = '18:00';
+        this.all_times = timeToArray(start, end, slot);
+
+    }
+
     // Get Appointments
     async getAppointment() {
         const appointment = await API.graphql(graphqlOperation(Queries.listAppointments, {
             filter: {
                 date: {
-                    contains: "2019-10-22"
+                    contains: '2019-10-22'
                 }
             }
         }));
-        
-        let appointments = await appointment.data.listAppointments.items;    
-        let reserved_time =[];
-        for( let $i = 0 ; $i < appointments.length ; $i++){
+
+        const appointments = await appointment.data.listAppointments.items;
+        const reserved_time = [];
+        for (let $i = 0; $i < appointments.length; $i++) {
             reserved_time.push(appointments[$i].start_time);
-        } 
-    
-        const available_time  = this.all_times.filter((item: any) => reserved_time.indexOf(item) < 0);
-        this.times = available_time;  
-    } 
+        }
+
+        const available_time = this.all_times.filter((item: any) => reserved_time.indexOf(item) < 0);
+        this.times = available_time;
+    }
 
     // Create Appointment
     createAppointment = async (time: any) => {
@@ -129,50 +137,41 @@ export class AppointmentPage implements OnInit {
             client_id: this.clientid,
             price: this.serviceprice,
             status: this.status,
-            date: "2019-10-22",
+            date: '2019-10-22',
             start_time: time,
             end_time: time + this.calender_offset,
         };
         await API.graphql(graphqlOperation(mutations.createAppointment, {input: appointment}));
         this.router.navigate(['service']);
-    };    
-} 
-   
+    };
+}
 
 
+// const App_Id = await API.graphql(graphqlOperation(`query MyQuery {getApp(id: "1") { settings { items {  start_time  } } } }`));
 
+// let start = this.start_times;
+// let end   = this.end_times;
+// let start   = moment(this.start_times).format('HH:mm').toString();
+// let end   = moment(this.end_times).format('HH:mm').toString();
+// let start =  moment(this.start_times).format('LT');
+// let end =  moment(this.end_times).format('LT');
+// console.log(start);
+// console.log(end);
 
-
-
-
-
-
-
-   // const App_Id = await API.graphql(graphqlOperation(`query MyQuery {getApp(id: "1") { settings { items {  start_time  } } } }`));
- 
-    // let start = this.start_times; 
-    // let end   = this.end_times;   
-    // let start   = moment(this.start_times).format('HH:mm').toString();
-    // let end   = moment(this.end_times).format('HH:mm').toString();
-    // let start =  moment(this.start_times).format('LT');
-    // let end =  moment(this.end_times).format('LT');
-    // console.log(start);
-    // console.log(end);
- 
-    // showDatepicker(){
-    //     this.datePicker.show({
-    //       date: new Date(),
-    //       mode: 'date',
-    //       androidTheme: this.datePicker.ANDROID_THEMES.THEME_HOLO_DARK,
-    //       okText:"Save Date",
-    //       todayText:"Set Today"
-    //     }).then(
-    //       date => {
-    //         this.myDate = date.getDate()+"/"+date.toLocaleString('default', { month: 'long' })+"/"+date.getFullYear();
-    //       },
-    //       err => console.log('Error occurred while getting date: ', err)
-    //     );
-    //   }  
+// showDatepicker(){
+//     this.datePicker.show({
+//       date: new Date(),
+//       mode: 'date',
+//       androidTheme: this.datePicker.ANDROID_THEMES.THEME_HOLO_DARK,
+//       okText:"Save Date",
+//       todayText:"Set Today"
+//     }).then(
+//       date => {
+//         this.myDate = date.getDate()+"/"+date.toLocaleString('default', { month: 'long' })+"/"+date.getFullYear();
+//       },
+//       err => console.log('Error occurred while getting date: ', err)
+//     );
+//   }
 
 
 
--- src/app/page/settings/settings.page.ts
+++ src/app/page/settings/settings.page.ts
@@ -1,65 +1,61 @@
 import {Component, OnInit} from '@angular/core';
-import {Router} from '@angular/router'; 
+import {Router} from '@angular/router';
 import {ModalController} from '@ionic/angular';
 import {AppId} from '../../AppId/Id';
 
 import {graphqlOperation, API} from 'aws-amplify';
 import * as Queries from '../../../graphql/queries';
-import * as mutations from '../../../graphql/mutations'; 
-import * as moment from 'moment/moment'; 
-
+import * as mutations from '../../../graphql/mutations';
+import * as moment from 'moment/moment';
 
 
 @Component({
-  selector: 'app-settings',
-  templateUrl: './settings.page.html',
-  styleUrls: ['./settings.page.scss'],
+    selector: 'app-settings',
+    templateUrl: './settings.page.html',
+    styleUrls: ['./settings.page.scss'],
 })
 export class SettingsPage implements OnInit {
-   
-  public settingid: any; // = 1 Temporarily
-  public start_time: any;
-  public end_time: any;
-  public calender_offset: any;  
 
-  public moment: any;
-  constructor(private modalController: ModalController, private router: Router) {
-    this.moment = moment();
-  }
+    public settingid: any; // = 1 Temporarily
+    public start_time: any;
+    public end_time: any;
+    public calender_offset: any;
 
-  async ngOnInit() { 
-      const setting = await API.graphql(graphqlOperation(Queries.getSetting, {id: 1})); 
-      this.settingid       = setting.data.getSetting.id; 
-      this.start_time      = setting.data.getSetting.start_time; 
-      this.end_time        = setting.data.getSetting.end_time;
-      this.calender_offset = setting.data.getSetting.calender_offset; 
-  }
- 
-  // Update Setting
-  updateSetting = async (form: { value: { start_time: any; end_time: any; calender_offset: any; }; }) => {
-      event.preventDefault();
-      const setting = { 
-          id: this.settingid, 
-          // start_time: moment(form.value.start_time).format('HH:mm').toString(),
-          // end_time: moment(form.value.end_time).format('HH:mm').toString(),
-          start_time: form.value.start_time,
-          end_time: form.value.end_time,
-          // start_time: moment(form.value.start_time).format('LT').toString(),
-          // end_time: moment(form.value.end_time).format('LT'),
-          calender_offset: form.value.calender_offset,
-          settingAppId: AppId,
-      };
-      console.log(setting); 
-      await API.graphql(graphqlOperation(mutations.updateSetting, {input: setting}));
-      this.router.navigate(['service']);
-  };    
+    public moment: any;
 
+    constructor(private modalController: ModalController, private router: Router) {
+        this.moment = moment();
+    }
 
+    async ngOnInit() {
+        const setting = await API.graphql(graphqlOperation(Queries.getSetting, {id: 1}));
+        this.settingid = setting.data.getSetting.id;
+        this.start_time = setting.data.getSetting.start_time;
+        this.end_time = setting.data.getSetting.end_time;
+        this.calender_offset = setting.data.getSetting.calender_offset;
+    }
 
+    // Update Setting
+    updateSetting = async (form: { value: { start_time: any; end_time: any; calender_offset: any; }; }) => {
+        event.preventDefault();
+        const setting = {
+        //   id: this.settingid,
+            // start_time: moment(form.value.start_time).format('HH:mm').toString(),
+            // end_time: moment(form.value.end_time).format('HH:mm').toString(),
+            start_time: form.value.start_time,
+            end_time: form.value.end_time,
+            // start_time: moment(form.value.start_time).format('LT').toString(),
+            // end_time: moment(form.value.end_time).format('LT'),
+            calender_offset: form.value.calender_offset,
+            settingAppId: AppId,
+        };
+        console.log(setting);
+        await API.graphql(graphqlOperation(mutations.createSetting, {input: setting}));
+        this.router.navigate(['service']);
+    };
 
 
-
-  normalizzaTime(data) {
-      return moment(data).format('HH:mm').toString();
-  }
+    normalizzaTime(data) {
+        return moment(data).format('HH:mm').toString();
+    }
 }
